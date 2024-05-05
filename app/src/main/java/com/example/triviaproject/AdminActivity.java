1003978424155389264fdec8945099ed606fa607
Kazuma Saito <kazu15870@gmail.com>
2024-05-04 17:58:17 -0700

    chore: #14 Deleted unnecessary code

--- app/src/main/java/com/example/triviaproject/AdminActivity.java
+++ app/src/main/java/com/example/triviaproject/AdminActivity.java
@@ -11,7 +11,6 @@ import com.example.triviaproject.databinding.ActivityAdminBinding;
 
 public class AdminActivity extends AppCompatActivity {
     private ActivityAdminBinding binding;
-    private static final String ADMIN_ACTIVITY_USER_ID = "com.example.triviaproject.ADMIN_ACTIVITY_USER_ID";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
