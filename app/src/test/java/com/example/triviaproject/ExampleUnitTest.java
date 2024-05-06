4c573650f5608254ce1257ea6c9ebb63022e6497
GLeTan <gordonletan@gmail.com>
2024-05-05 22:31:35 -0700

    Unit test for user created

--- app/src/test/java/com/example/triviaproject/ExampleUnitTest.java
+++ app/src/test/java/com/example/triviaproject/ExampleUnitTest.java
@@ -1,8 +1,25 @@
 package com.example.triviaproject;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+
+import com.example.triviaproject.database.TriviaRepository;
+import com.example.triviaproject.database.entities.User;
+import com.example.triviaproject.databinding.ActivityRegisterBinding;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +27,53 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    User u1 = null;
+    String defaultUserName = "Sample";
+    String defaultPassWord = "Text";
+    boolean defaultAdmin = false;
+    @Before
+    public void setUp(){
+        u1 = new User(defaultUserName,defaultPassWord,defaultAdmin);
     }
+    @After
+    public void tearDown(){
+        u1 = null;
+    }
+    @Test
+    public void account_test(){
+        User u2 = null;
+        assertNull(u2);
+        u2 = new User("Gordon", "Freeman", false);
+        assertNotNull(u2);
+        assertNotNull(u1);
+    }
+
+    @Test
+    public void getAccount_test(){
+        assertEquals(defaultUserName,u1.getUserName());
+        assertEquals(defaultPassWord,u1.getPassword());
+        assertEquals(defaultAdmin,u1.isAdmin());
+    }
+
+    @Test
+    public void setAccount_test(){
+        assertEquals(defaultUserName,u1.getUserName());
+        String newName = "test";
+        u1.setUserName(newName);
+        assertNotEquals(defaultUserName,u1.getUserName());
+        assertEquals(newName,u1.getUserName());
+
+        assertEquals(defaultPassWord,u1.getPassword());
+        String newPassWord = "chamber";
+        u1.setPassword(newPassWord);
+        assertNotEquals(defaultPassWord, u1.getPassword());
+        assertEquals(newPassWord,u1.getPassword());
+
+        assertEquals(defaultAdmin,u1.isAdmin());
+        boolean newAdmin = true;
+        u1.setAdmin(newAdmin);
+        assertNotEquals(defaultAdmin, u1.isAdmin());
+        assertEquals(newAdmin,u1.isAdmin());
+    }
+
 }
